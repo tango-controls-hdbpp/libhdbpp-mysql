3a1c462dcbf72914959d6db3cfe74503c17ba9b7
gscalamera <graziano.scalamera@elettra.eu>
2021-05-18 15:32:00 +0200

    Remove redundant MySQL commit

--- src/LibHdb++MySQL.cpp
+++ src/LibHdb++MySQL.cpp
@@ -894,7 +894,7 @@ void HdbPPMySQL::insert_events(vector<tuple<Tango::EventData *, HdbEventDataType
 		throw;
 	}
 	mysql_autocommit(dbp, 1);
-	mysql_commit(dbp); //TODO: NEEDED?
+	//mysql_commit(dbp); //TODO: NEEDED?
 #ifdef _LIB_DEBUG
 	//cout << __func__<< ": exiting... "<< endl;
 #endif
